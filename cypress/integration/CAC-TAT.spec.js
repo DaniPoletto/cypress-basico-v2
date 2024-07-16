3fa64c92167e36e3b12bd3484a5ca1fd4b03688a
DaniPoletto <danielapoletto_bep@hotmail.com>
2024-07-16 08:31:17 -0300

    feat - encontrar o gato

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -244,5 +244,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             })
     })
 
+    it.only('encontra o gato escondido', function() {
+        cy.get('#cat')
+            .invoke('show')
+            .should('be.visible')
+        cy.get('#title')
+            .invoke('text', 'CAT TAT')
+    })
+
   })
   
